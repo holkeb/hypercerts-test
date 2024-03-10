e3dc4526056e0ac409b50264b429d3ec350ba57a
holkeb <holke.brammer@gmail.com>
2024-03-10 01:13:11 +0100

    bring hyperboard to separate page

--- src/pages/ecosystem.js
+++ src/pages/ecosystem.js
@@ -3,41 +3,60 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import HyperboardWidget from '@site/src/components/HyperboardWidget';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      title="hypercerts overview"
+      description="Hypercerts are the web3 standard for impact certificates <head />">
+      <div style={{
+          display: 'flex',
+          flexDirection: 'column', // Stack children horizontally
+          alignItems: 'center', // Align children vertically in the middle
+          justifyContent: 'space-between', // Distribute extra space
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginTop: '100px',
+          padding: '0 50px',
+          maxWidth: '1080px',
+        }}>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row', // Stack children horizontally
+          alignItems: 'center', // Align children vertically in the middle
+          justifyContent: 'space-between', // Distribute extra space
+        }}>
+          <div style={{flex: 1, paddingRight: '2rem' }}> {/* Text content on the left */}
+            <h1 class="title-font">Reward impact with hypercerts</h1>
+            <p class="text-font">
+              To solve the challenges we face this century, we need scalable and
+              sustainable financing models for public goods that reward
+              contributors for the positive impact they create.
+            </p>
+            <div>
+              <Link to="/ecosystem" className="primaryButton">Launch app</Link>
+              <Link to="/ecosystem" className="secondaryButton">Read the docs</Link>
+            </div>
+          </div>
+          <div style={{ flex: 1 }}> {/* Image placeholder on the right */}
+            <div style={{
+              border: '0px solid #eee',
+              borderRadius: '8px',
+              padding: '1rem',
+              textAlign: 'center',
+              minHeight: '200px', // Adjust height as necessary
+            }}>
+              <img src="/img/hypercert_example_shadow.png" alt="Hypercerts" style={{ maxWidth: '300px', maxHeight: '100%' }} />
+            </div>
+          </div>    
+        </div>
+      </div>
     </Layout>
   );
 }
+
+export default HomepageHeader;
